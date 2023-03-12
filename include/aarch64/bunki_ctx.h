d8757a7cc0d7a3162e9fd8ad144038d8b590c4b8
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2023-03-12 15:55:45 -0700

    Header for aarch64

---
+++ include/aarch64/bunki_ctx.h
@@ -0,0 +1,31 @@
+#ifndef BUNKI_AARCH64_CTX_FILE_H
+#define BUNKI_AARCH64_CTX_FILE_H
+
+#if !defined(__aarch64__)
+    #error "This is not an ARM64 architecture"
+#endif
+
+#include <stdint.h>
+
+struct stack_ctx_s {
+    uint64_t lr;
+    uint64_t x29;
+    uint64_t x28;
+    uint64_t x27;
+    uint64_t x26;
+    uint64_t x25;
+    uint64_t x24;
+    uint64_t x23;
+    uint64_t x22;
+    uint64_t x21;
+    uint64_t x20;
+    uint64_t x19;
+};
+
+#define ARCH_STK_ALIGN 0x10U
+
+unsigned  bunki_patch_call_yield(uint32_t stack_size);
+bunki_t   bunki_native_finalize_ctx(bunki_t ctx, uintptr_t (*func)(void*), void* arg, uintptr_t stack_end);
+uintptr_t bunki_ctx_stack_start(void);
+
+#endif
